869ff1932d4a96d3fba372720c5e65fa7b6502a0
Chung Leong <chernyshevsky@hotmail.com>
2021-09-30 22:12:47 +0200

    Implemented loading animation (issue #1).

--- article.js
+++ article.js
@@ -3,17 +3,27 @@ import { generateFootnoteContents } from './lib/translation.js';
 import { addText,  addFootnotes } from './lib/layout.js';
 
 async function start() {
+  const setStatus = (status) => document.body.className = status;
+  // show loader only when loading takes a while
+  let done = false;
+  setTimeout(() => done || setStatus('pending'), 250);
   try {
-    // look up definitions
+    const title = 'This is a test';
     const content = sampleText;
+    document.title = title;
+    // look up definitions
     const footnotes = await generateFootnoteContents(content);
     // add the article text into the DOM
     addText(content);
     // add footnotes
     addFootnotes(footnotes);
+    setStatus('ready');
   } catch (e) {
-    console.error(e);
+    const errorElement = document.getElementById('error-text');
+    errorElement.textContent = e.toString();
+    setStatus('error');
   }
+  done = true;
 }
 
 addEventListener('load', start);
